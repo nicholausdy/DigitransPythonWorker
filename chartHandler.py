86eabc97549fa3d64feccdc189aee5e0574aeb60
narberal <nicdanyos@gmail.com>
2021-01-26 20:01:54 +0700

    Add horizontal bar

--- chartHandler.py
+++ chartHandler.py
@@ -60,14 +60,56 @@ def renderPie(chartInfoObject):
     print(error)
     raise Exception(error)
 
-async def asyncRenderHandler(questionnaireId, questionId):
+def renderBar(chartInfoObject):
+  try:
+    # styling
+    custom_style = Style(
+      title_font_size=2,
+      legend_font_size=2,
+      tooltip_font_size=2   #hover element
+    )
+    bar_chart = pygal.Bar(
+      width=110, 
+      height=70, 
+      margin_top=3,
+      spacing = 7,
+      legend_box_size=1, 
+      tooltip_border_radius=0.5,
+      style = custom_style)
+    bar_chart.title = chartInfoObject['questionDesc']
+    for i in range(len(chartInfoObject['optionInfo'])):
+      optionObj = chartInfoObject['optionInfo'][i]
+      bar_chart.add(optionObj['description'], optionObj['number_chosen'])
+    filenameQuestionnairePart = chartInfoObject['optionInfo'][i]['questionnaire_id'].replace("/","")
+    filenameQuestionnairePart =filenameQuestionnairePart.replace(".","")
+    filenameQuestionPart = str(chartInfoObject['optionInfo'][i]['question_id'])
+    directory = os.getenv("CHART_DIR") + 'chart-' + filenameQuestionnairePart + '-' + filenameQuestionPart + '.svg'
+    bar_chart.render_to_file(directory, force_uri_protocol='http')
+    return directory
+  except Exception as error:
+    print(error)
+    raise Exception(error)
+
+def chartSelector(chartInfoObject, chartType):
+  try:
+    dictSelector = {
+      "pie": renderPie,
+      "bar": renderBar
+    }
+    directory = dictSelector[chartType](chartInfoObject)
+    return directory
+  except Exception as error:
+    print(error)
+    raise Exception(error)
+
+async def asyncRenderHandler(questionnaireId, questionId, chartType):
   try:
     result = {}
     chartInfoObject = await getChartInfo(questionnaireId, questionId)
     if (not(chartInfoObject['success'])):
       result['success'] = False
     else:
-      result['directory'] = await asyncTransform(renderPie, chartInfoObject)
+      result['directory'] = await asyncTransform(chartSelector, chartInfoObject, chartType)
       result['success'] = True
     return result
   except Exception as error:
